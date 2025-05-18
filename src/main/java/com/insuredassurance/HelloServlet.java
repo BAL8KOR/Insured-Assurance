85b3ef6535073c0134453e51a66b249e846d2cd1
Shilpa <shilpabala.balasubramaniam@in.bosch.com>
2025-05-18 13:31:20 +0530

    Java Application project

---
+++ src/main/java/com/insuredassurance/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.insuredassurance;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Welcome to Insured Assurance Web App</h1>");
+        out.println("<p>This servlet is deployed on Apache Tomcat using Jenkins & GitHub Actions CI/CD.</p>");
+    }
+}
